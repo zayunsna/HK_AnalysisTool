4faf1b521927a642a7cad8476160b6b3aacc0204
zayunsna <zayunsna@gmail.com>
2023-12-06 12:56:31 +0900

    update IF, add RCF(AWS) and RRCF

---
+++ RRCF.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.ensemble import IsolationForest
+import rrcf
+from gen_toyData import DataFrameGenerator
+
+num_rows = 5000
+frequency = 1/1300
+frequency_std = 1/1000
+ratio_accidental = 0.001
+n_features = 4
+
+generator = DataFrameGenerator(num_rows = num_rows,
+                               frequency = frequency,
+                               frequency_std = frequency_std,
+                               ratio_accidental = ratio_accidental,
+                               n_features = n_features)
+# generator.add_accidental_high_values()
+df = generator.gen_df()
+plt.figure(figsize=(12,8))
+plt.plot(df[['Feature_1']])
+plt.grid()
+plt.show()
+X = df['Feature_1'].astype(float).values
+
+num_trees = 200
+shingle_size = 48
+tree_size = 1000
+
+points = rrcf.shingle(X, size=shingle_size)
+points = np.vstack([point for point in points])
+n = points.shape[0]
+sample_size_range = (n // tree_size, tree_size)
+
+forest = []
+while len(forest) < num_trees:
+    ixs = np.random.choice(n, size=sample_size_range,
+                           replace=False)
+    trees = [rrcf.RCTree(points[ix], index_labels=ix) for ix in ixs]
+    forest.extend(trees)
+
+avg_codisp = pd.Series(0.0, index=np.arange(n))
+index = np.zeros(n)
+
+for tree in forest:
+    codisp = pd.Series({leaf : tree.codisp(leaf) for leaf in tree.leaves})
+    avg_codisp[codisp.index] += codisp
+    np.add.at(index, codisp.index.values, 1)
+    
+avg_codisp /= index
+avg_codisp.index = df.iloc[(shingle_size-1):].index
+
+
+############# Isolation Forest for the comparison
+contamination = ratio_accidental
+IF = IsolationForest(n_estimators=num_trees,
+                     contamination=contamination,
+                     max_samples='auto',
+                     random_state=0)
+IF.fit(df[['Feature_1']])
+if_scores = IF.decision_function(df[['Feature_1']])*-1
+if_anomaly = IF.predict(df[['Feature_1']])
+df['if_anomaly'] = if_anomaly
+#############
+
+avg_codisp = ((avg_codisp - avg_codisp.min()) / (avg_codisp.max() - avg_codisp.min()))
+if_scores = ((if_scores - if_scores.min()) / (if_scores.max() - if_scores.min()))
+
+df['rrcf_scores'] = avg_codisp
+df['if_scores'] = if_scores
+
+score_mean = df['rrcf_scores'].mean()
+score_std = df['rrcf_scores'].std()
+score_based_threshold = score_mean + 3 * score_std
+over_threshold = df['rrcf_scores'] > score_based_threshold
+
+
+fig, ax = plt.subplots(2, figsize=(12,8))
+ax[0].plot(df['Feature_1'], color='0.5', label = 'Pseudo Data')
+ax[0].scatter(df.index[df['if_anomaly'] == -1], df['Feature_1'][df['if_anomaly'] == -1], color='springgreen', label='Anomaly [IF]')
+ax[0].scatter(df.index[over_threshold], df['Feature_1'][over_threshold], color='red', marker='*', alpha=0.6, label='Anomaly [RRCF]')
+ax[1].plot(df['if_scores'], color='#7EBDE6', alpha=0.8, label='Score [IF]')
+ax[1].plot(df['rrcf_scores'], color='#E8685D', alpha=0.8, label='Score [RRCF]')
+
+ax[0].legend(frameon=True, loc=2, fontsize=12)
+ax[0].set_ylabel('Pseudo data Test', size=13)
+ax[0].set_title('Anomaly detection Test', size=14)
+ax[1].legend(frameon=True, loc=2, fontsize=12)
+ax[1].set_ylabel('Normalized Anomaly Score', size=13)
+
+ax[0].xaxis.set_ticklabels([])
+ax[0].grid()
+ax[1].grid()
+ax[0].set_xlim(df.index[0], df.index[-1])
+ax[1].set_xlim(df.index[0], df.index[-1])
+plt.tight_layout()
+
+plt.show()
--- isolationForest.py
+++ isolationForest.py
@@ -1,69 +1,115 @@
-# #!/usr/bin/env python
-# # -*- coding: utf-8 -*-
-# import pandas as pd
-# import numpy as np
-# import matplotlib as mpl
-# import matplotlib.pyplot as plt
-# from matplotlib.patches import Ellipse
-# from sklearn.preprocessing import MinMaxScaler, StandardScaler
-# from sklearn.datasets import make_blobs
-# from gen_toyData import DataFrameGenerator
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.datasets import make_blobs
+from gen_toyData import DataFrameGenerator
 
 
-# generator = DataFrameGenerator(num_rows=5000, frequency=1/1300, frequency_std=1/1000, ratio_accidental=0.001, n_features=4)
-# # generator.add_accidental_high_values()
-# df = generator.gen_df()
+generator = DataFrameGenerator(num_rows=50000, frequency=1/1300, frequency_std=1/1000, ratio_accidental=0.001, n_features=4)
+# generator.add_accidental_high_values()
+df = generator.gen_df()
 
-# plt.figure(figsize=(12,8))
-# plt.plot(df[['Feature_1']])
-# plt.grid()
-# plt.show()
+plt.figure(figsize=(12,8))
+plt.plot(df[['Feature_1']])
+plt.grid()
+plt.show()
 
-# df = df[['Feature_1']]
+df = df[['Feature_1']]
 
-# from sklearn.ensemble import IsolationForest
+from sklearn.ensemble import IsolationForest
+from sklearn.model_selection import train_test_split
 
-# randseed = np.random.RandomState(42)
 
-# # Fit the model
-# clf = IsolationForest(contamination=0.05, random_state=randseed)
-# clf.fit(df[['Feature_1']])
+randseed = np.random.RandomState(42)
 
-# # Predict anomalies
+# # Splitting the dataset into training and testing sets
+# train_df, test_df = train_test_split(df, test_size=0.3, random_state=42)
+
+# # Isolation Forest
+# clf = IsolationForest(n_estimators=100, max_samples='auto', contamination=float(.02), random_state=42)
+# clf.fit(train_df[['Feature_1']])
+
+# # Anomaly Scoring
 # df['scores'] = clf.decision_function(df[['Feature_1']])
 # df['anomaly'] = clf.predict(df[['Feature_1']])
 
-# # Visualize
+
+# Fit the model
+clf = IsolationForest(contamination=0.001, random_state=randseed)
+clf.fit(df[['Feature_1']])
+
+# Predict anomalies
+df['scores'] = clf.decision_function(df[['Feature_1']])
+df['anomaly'] = clf.predict(df[['Feature_1']])
+
+# Visualize
+fig, ax1 = plt.subplots(figsize=(12,8))
+ax2 = ax1.twinx()
+ax1.plot(df[['Feature_1']], color='blue', label='Data points[line]')
+ax2.plot(df[['scores']], color = 'Orange', alpha=0.5, label='Score')
+ax1.scatter(df.index, df['Feature_1'], color='k', s=3., label='Data points')
+ax1.scatter(df.index[df['anomaly'] == -1], df['Feature_1'][df['anomaly'] == -1], color='red', label='Anomaly')
+fig.legend()
+plt.show()
+
+# from sklearn.ensemble import IsolationForest
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from sklearn.datasets import make_blobs
+
+# # Generating a synthetic dataset
+# np.random.seed(42)
+# X, _ = make_blobs(n_samples=300, centers=1, cluster_std=1, center_box=(-10.0, 10.0))
+# X = np.concatenate([X, np.random.uniform(low=-10, high=10, size=(20, 2))])  # adding some anomalies
+
+# # Applying Isolation Forest
+# iso_forest = IsolationForest(contamination=0.06)
+# preds = iso_forest.fit_predict(X)
+
+# # Visualizing the results
 # plt.figure(figsize=(10, 6))
-# plt.plot(df[['Feature_1']], color='blue', label='Data points[line]')
-# plt.scatter(df.index, df['Feature_1'], color='k', s=3., label='Data points')
-# plt.scatter(df.index[df['anomaly'] == -1], df['Feature_1'][df['anomaly'] == -1], color='red', label='Anomaly')
+# plt.scatter(X[:, 0], X[:, 1], c=preds, cmap='coolwarm', marker='o', s=35, edgecolor='k', label='Data Points')
+# plt.title('Isolation Forest Anomaly Detection')
+# plt.xlabel('Feature 1')
+# plt.ylabel('Feature 2')
 # plt.legend()
-# plt.title("Anomaly Detection in Time Series Data")
-# plt.xlabel("Time")
-# plt.ylabel("Value")
+# plt.grid(True)
 # plt.show()
 
-from sklearn.ensemble import IsolationForest
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.datasets import make_blobs
+# # Our tiny dataset
+# X_tiny = np.array([[5, 7], [6, 9], [8, 8], [9, 6], [15, 15]])
 
-# Generating a synthetic dataset
-np.random.seed(42)
-X, _ = make_blobs(n_samples=300, centers=1, cluster_std=1, center_box=(-10.0, 10.0))
-X = np.concatenate([X, np.random.uniform(low=-10, high=10, size=(20, 2))])  # adding some anomalies
+# # Function to build an isolation tree
+# def build_isolation_tree(X, depth=0, max_depth=2):
+#     n_samples = X.shape[0]
+#     if depth == max_depth or n_samples <= 1:
+#         return np.array([depth])
 
-# Applying Isolation Forest
-iso_forest = IsolationForest(contamination=0.06)
-preds = iso_forest.fit_predict(X)
+#     # Randomly select a feature and a split value
+#     feature_idx = np.random.randint(0, X.shape[1])
+#     feature_vals = X[:, feature_idx]
+#     split_val = np.random.uniform(np.min(feature_vals), np.max(feature_vals))
 
-# Visualizing the results
-plt.figure(figsize=(10, 6))
-plt.scatter(X[:, 0], X[:, 1], c=preds, cmap='coolwarm', marker='o', s=35, edgecolor='k', label='Data Points')
-plt.title('Isolation Forest Anomaly Detection')
-plt.xlabel('Feature 1')
-plt.ylabel('Feature 2')
-plt.legend()
-plt.grid(True)
-plt.show()
+#     # Split the dataset
+#     left_indices = X[:, feature_idx] < split_val
+#     right_indices = ~left_indices
+
+#     # Recursively build the left and right branches
+#     left_tree = build_isolation_tree(X[left_indices], depth + 1, max_depth)
+#     right_tree = build_isolation_tree(X[right_indices], depth + 1, max_depth)
+
+#     # Return the tree structure
+#     return np.array([depth, feature_idx, split_val, left_tree, right_tree], dtype=object)
+
+# # Building two simple isolation trees
+# tree1 = build_isolation_tree(X_tiny, max_depth=3)
+# tree2 = build_isolation_tree(X_tiny, max_depth=3)
+
+# # Displaying the structure of the trees
+# print(tree1)
+# print(tree2)
